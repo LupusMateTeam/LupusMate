12fb841c0ffa65024ff1e628d06d17466b09b611
Nithya Konda <nithya.konda001@umb.edu>
2014-12-03 12:53:38 -0500

    Adding Project

---
+++ src/com/cs680/lupusmate/activity/Welcome.java
@@ -0,0 +1,18 @@
+package com.cs680.lupusmate.activity;
+
+import com.cs680.lupusmate.R;
+import com.cs680.lupusmate.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Welcome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
+}
